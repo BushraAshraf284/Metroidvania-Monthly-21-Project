699826e2619bafad7366821aa4ddf7a24b486640
Travp333 <72285478+Travp333@users.noreply.github.com>
2023-08-15 21:59:20 -0700

    Started general level layout and design, imported starting points from old projects
    I added in a map i drew sketching out a general layout of a potential test level. I then traced over it with probuilder. I plan to use this same workflow on the main levels, except I will then export the probuilder mesh and make it more interesting and consistent in blender. I also added in the general movement system, as well as a test character to stand in until I finish the mech model and animations. This character also had the rebindable controls, settings, and pause menus as dependencies so I imported them all in as well. I updated the menus and scripts to only be relevant to this jam.

---
+++ Assets/Scripts/UI/Controls.cs
@@ -0,0 +1,69 @@
+//Author: Brian Meginness + Travis Parks
+//Debugging: Brian Meginness
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public Dictionary<string, KeyCode> keys;
+    public Dictionary<KeyCode, bool> inUse;
+
+    public static bool exists = false;
+
+    private void Start()
+    {
+        exists = true;
+    }
+
+    private void Awake()
+    {
+        if (!exists)
+        {
+            GameObject.DontDestroyOnLoad(this);
+            //A dictionary containing game actions and associated keys
+            keys = new Dictionary<string, KeyCode>()
+            {
+                {"walkUp",KeyCode.W},
+                {"walkDown",KeyCode.S},
+                {"walkLeft",KeyCode.A},
+                {"walkRight",KeyCode.D},
+                {"jump",KeyCode.Space},
+                {"dash",KeyCode.LeftShift},
+                {"interact",KeyCode.F},
+                {"attack",KeyCode.Mouse0},
+                {"aim",KeyCode.Mouse1},
+                {"leftweaponmenu",KeyCode.E},
+                {"rightweaponmenu",KeyCode.Q}
+            };
+
+            //Dictionary for what keys on the keyboard are in use
+            inUse = new Dictionary<KeyCode, bool>();
+
+            //FOR all possible keys, set to not in use
+            foreach (KeyCode key in System.Enum.GetValues(typeof(KeyCode)))
+            {
+                try
+                {
+                    inUse.Add(key, false);
+                }
+                catch
+                {
+
+                }
+
+            }
+
+            //FOR each key being used, set in use to true
+            foreach (KeyCode key in keys.Values)
+            {
+                inUse[key] = true;
+            }
+
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
