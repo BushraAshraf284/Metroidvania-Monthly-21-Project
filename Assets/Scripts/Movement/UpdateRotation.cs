b9e3a6d4a571be65315a69d28ab08dbf5ba34f2e
Travp333 <72285478+Travp333@users.noreply.github.com>
2023-08-20 18:36:54 -0700

    smol tweak to clear some warnings

--- Assets/Scripts/Movement/UpdateRotation.cs
+++ Assets/Scripts/Movement/UpdateRotation.cs
@@ -22,7 +22,6 @@ public class UpdateRotation : MonoBehaviour
     Movement sphere; 
     Rigidbody body;
     Vector3 DummyGrav;
-    bool Gate = true;
     bool gravSwap;
 	[SerializeField]
 	public bool isAiming;
@@ -35,7 +34,6 @@ public class UpdateRotation : MonoBehaviour
 	Camera controllingCam;
 	[SerializeField]
 	float cameraBlendRate = 500f;
-	float camBlender = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -46,12 +44,8 @@ public class UpdateRotation : MonoBehaviour
         sphere = player.GetComponent<Movement>();
         body = player.GetComponent<Rigidbody>();
     }
-//	void resetGate(){
-	//	Gate = false;
-	//}
 
     void Update() {
-        //transform.Rotate(0,5,0);
 		if(aimingCamera.GetComponent<OrbitCamera>() != null){
 			if(isAiming){
 				if(controllingCam.fieldOfView > aimingCamera.GetComponent<OrbitCamera>().aimFOV){
@@ -76,14 +70,8 @@ public class UpdateRotation : MonoBehaviour
 				}
 
 				if(this.transform.up != CustomGravity.GetUpAxis(this.transform.position)){
-					//Debug.Log("Gravity Mismatch!");
-					//if(Gate == false){
 						Quaternion toRotation = Quaternion.LookRotation( transform.forward , CustomGravity.GetUpAxis(this.transform.position) );
 						this.transform.rotation = Quaternion.RotateTowards (transform.rotation, toRotation, (gravRotationSpeed) * Time.deltaTime);
-					//}
-					//else{
-					//	Invoke("resetGate", .3f);
-					//}
 				}
 				else{
 					Quaternion toRotation2 = Quaternion.LookRotation(ProjectDirectionOnPlane(aimingCamera.transform.forward, CustomGravity.GetUpAxis(this.transform.position)), CustomGravity.GetUpAxis(this.transform.position));
@@ -126,17 +114,8 @@ public class UpdateRotation : MonoBehaviour
 		Debug.DrawRay(this.transform.position, player2Pointer, Color.gray, 3f);
         Vector3 gravity = CustomGravity.GetUpAxis(this.transform.position);
 		if(this.transform.up != CustomGravity.GetUpAxis(this.transform.position)){
-			//Debug.Log("Gravity Mismatch!");
-		//	if(Gate == false){
 				Quaternion toRotation = Quaternion.LookRotation( transform.forward , CustomGravity.GetUpAxis(this.transform.position) );
 				this.transform.rotation = Quaternion.RotateTowards (transform.rotation, toRotation, (gravRotationSpeed) * Time.deltaTime);
-		//	}
-		//	else{
-		//		Invoke("resetGate", .3f);
-		//	}
-		}
-		else{
-			Gate = true;
 		}
 		if (sphere.velocity.magnitude > .2f && (sphere.playerInput != Vector3.zero)){
 			if(!sphere.OnGround){
