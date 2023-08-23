0f3f3cc622b0ed8ab0d652a019c0c57bfd784b6b
Travp333 <Travp333@yahoo.com>
2023-08-23 12:56:15 -0700

    added UI icons and did some basic implementation

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,4 +1,4 @@
-//adapted and modified from video "How to make a HEALTH BAR in Unity!" by Brackeys
+﻿//adapted and modified from video "How to make a HEALTH BAR in Unity!" by Brackeys
 //https://www.youtube.com/watch?v=BLfNP4Sc_iA
 //Author: Sandeep and Travis
 using System.Collections;
@@ -7,8 +7,22 @@ using UnityEngine;
 //this script will just keep track of the player's various stats and allow other scripts to access and edit them
 public class PlayerStats : MonoBehaviour
 {
-    public float hp = 100;
-    public HealthBar healthBar;
+	public float MaxHp = 100f;
+	public float hp = 100;
+	public float MaxCharge = 100f;
+	public float charge = 100;
+	
+	public HealthBar healthBar;
+	public HealthBar healthBar2;
+	public HealthBar healthBar3;
+	public BatteryBar batteryBar;
+	public BatteryBar batteryBar2;
+	public BatteryBar batteryBar3;
+	
+	public enum HPPhase{Phase1, Phase2, Phase3};
+	public HPPhase healthphase;
+	public enum BAPhase{Phase1, Phase2, Phase3};
+	public BAPhase batteryphase;
 
     public void restoreHP(float healAmount){
         if (hp + healAmount > 100){
@@ -18,6 +32,21 @@ public class PlayerStats : MonoBehaviour
             hp += healAmount;
         }
     }
+    
+	public void DrainBattery(float drain){
+		if (charge - drain < 0){
+			//Debug.Log("Went from "+hp+" to 0");
+			charge = 0;
+
+		}
+		else {
+
+			//Debug.Log("Went from "+hp+" to "+ Mathf.Round(hp-damage));
+			if(charge != Mathf.Round(charge-drain)){
+				charge = Mathf.Round(charge-drain);
+			}
+		}
+	}
 
     public void takeDamage(float damage){
         if (hp - damage < 0){
@@ -35,7 +64,9 @@ public class PlayerStats : MonoBehaviour
     }
 
     void Start()
-    {
+	{
+		//WAS WORKING ON IMPLEMENTING DIFFERENT PHASES
+		if(healthphase)
         //Test line to see if we can set a default start point
         //when game is started, it sets the slider max value to hp value
         healthBar.SetMaxHealth(hp);
---
+++ Assets/Scripts/UI/BatteryBar.cs
@@ -0,0 +1,27 @@
+﻿//adapted and modified from video "How to make a HEALTH BAR in Unity!" by Brackeys
+//https://www.youtube.com/watch?v=BLfNP4Sc_iA
+//Author: Sandeep
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BatteryBar : MonoBehaviour
+{
+	public Slider batterySlider;
+
+  //method used in PlayerStats class to set the max value of the slider
+	public void SetMaxCharge(float charge)
+  {
+    //sets max value of health equal to health
+	  batterySlider.maxValue = charge;
+    //sets current value of health equal to health
+    batterySlider.value = charge;
+
+  }
+  //method used in the PlayerStats class to set the current value of health to the slider
+	public void SetCharge(float charge)
+  {
+    batterySlider.value = charge;
+  }
+}
