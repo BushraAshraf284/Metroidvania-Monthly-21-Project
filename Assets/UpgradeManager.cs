bee797e15804c607232c30e4fb5ebad5ba157e70
Travp333 <72285478+Travp333@users.noreply.github.com>
2023-09-05 16:58:17 -0700

    made upgrade manager

---
+++ Assets/UpgradeManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour
+{
+    [SerializeField]
+    public List<GameObject> Upgrades = new List<GameObject>();
+
+}
